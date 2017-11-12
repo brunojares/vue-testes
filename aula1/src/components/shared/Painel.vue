b1fcc77373fe0a08081aadb6ba53eda71cad83c9
brunojares <contato.jares@gmail.com>
2017-11-12 20:14:00 -0300

    ok

---
+++ aula1/src/components/shared/Painel.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="painel">
+        <div class="titulo">{{ titulo }}</div>
+        <slot class="conteudo">          
+        </slot>         
+    </div>
+</template>
+<<style>
+ul.lista li div.painel{
+  text-align: center;
+}
+ul.lista div.painel .titulo{
+  border: solid 2px;
+  background: lightblue;
+  margin: 0 0 15px 0;
+  padding: 10px;
+  text-transform: uppercase;
+}
+</style>
+
+<<script>
+export default {
+    props: [
+        'titulo'
+    ]  
+}
+</script>
+
