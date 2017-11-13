610fe4bb6368635270d29554c20aeb467741bfc1
Bruno Jares <Jares@iccar.local>
2017-11-13 16:05:07 -0300

    transacao

--- aula1/src/components/shared/Painel.vue
+++ aula1/src/components/shared/Painel.vue
@@ -1,9 +1,11 @@
 <template>
     <div class="painel" @dblclick="visivel = !visivel">
         <div class="titulo">{{ titulo }}</div>
+        <transition name="fade">
         <div class="conteudo" v-show="visivel">
             <slot></slot>         
         </div>
+        </transition>
     </div>
 </template>
 <style>
@@ -16,6 +18,13 @@ ul.lista div.painel .titulo{
   margin: 0 0 15px 0;
   padding: 10px;
   text-transform: uppercase;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0
 }
 </style>
 
