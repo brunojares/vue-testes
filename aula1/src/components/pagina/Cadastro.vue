53ab51949a0cab05e0b8972200967b5844dbe8e8
brunojares <contato.jares@gmail.com>
2017-11-15 16:53:10 -0300

    Cadastro iniciado

--- aula1/src/components/pagina/Cadastro.vue
+++ aula1/src/components/pagina/Cadastro.vue
@@ -1,13 +1,87 @@
 <template>
     <div>
-        <div class="titulo">Cadastro</div>        
+        <h1 class="titulo">Cadastro</h1>        
+        <h2 class="titulo">{{ item.titulo }}</h2>
+        <form @submit.prevent="grava()">
+            <div class="controle">
+                <label for="titulo">Titulo</label>                
+                <input name="titulo" id="titulo" 
+                    autocomplete="off"
+                    v-model="item.titulo"
+                >
+            </div>            
+            <div class="controle">
+                <label for="url">Url</label>
+                <input name="url" id="url" 
+                    autocomplete="off"
+                    v-model.lazy="item.url"
+                >
+                <img class="miniatura" :src="item.url"  :alt="item.titulo" v-show="item.url"/>                
+            </div>                        
+            <div class="controle">
+                <label for="descricao">Descrição</label>
+                <textarea 
+                    name="descricao" id="descricao" 
+                    autocomplete="off" 
+                    cols="30" rows="10"
+                    v-model="item.descricao"
+                ></textarea>
+            </div>
+            <div class="centralizado">
+                <cmp-botao rotulo="Gravar" estilo="perigo" tipo="submit"/>
+                <router-link to="/"><cmp-botao rotulo="Voltar" tipo="button"></cmp-botao></router-link>
+            </div>
+        </form>
     </div>
 </template>
-<style>
-
-</style>
 <script>
-export default {
+import Botao from '../shared/Botao.vue';
 
+export default {
+    components:{
+        'cmp-botao': Botao
+    },
+    data(){
+        return {
+            item:{
+                titulo: '',
+                url: '',
+                descricao: ''
+            }
+        };
+    },
+    methods:{
+        grava(){
+            console.log(this.item);
+            this.item = {
+                titulo: '',
+                url: '',
+                descricao: ''
+            };
+        }
+    }
 }
 </script>
+<style>
+.controle{
+    font-size: 1.2em;
+    margin-bottom: 20px;
+}
+.controle label{
+    display: block;
+    font-weight: bold;
+}
+.controle label + input, .controle textarea{
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px;
+}
+.centralizado{
+    text-align: center;
+}
+img.miniatura{
+  width: 200px;
+  margin-top: 10px;
+}
+</style>
+
